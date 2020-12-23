c6e5fc1a8c5c5a2b00457027b7e16decf78f3265
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-12-23 09:03:08 +0530

    used docxJs library to generate document

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var documentController = require('../Controllers/document');
+var documentController = require('../Controllers/document2');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/document',documentController.document);
+router.get('/document',documentController.document2);
 
 module.exports = router;
 
